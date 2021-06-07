7293710bc45eac4f2a1b36ac1951e4381c3b5325
smigueles <smigueles@widergy.com>
2021-06-07 16:55:55 -0300

    nav style

--- App.js
+++ App.js
@@ -23,8 +23,21 @@ const App = () => {
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={Display} />
-          <Stack.Screen name="History" component={Record} />
+          <Stack.Screen
+            name="Home"
+            options={{
+              title: 'Calculator',
+              headerStyle: {
+                backgroundColor: '#e8d5b5',
+              },
+            }}
+            component={Display}
+          />
+          <Stack.Screen
+            name="History"
+            component={Record}
+            options={{headerStyle: {backgroundColor: '#e8d5b5'}}}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
